ad3e9392ea5cefd56418e5caa4775654e5a549c6
FieryXJoe <josephmsherry@comcast.net>
2021-03-03 07:54:47 -0500

    Step 1 of chat page
    Submission, selection of mod to receive new message and using data from fetched thread not implemented. Only redirect GET stuff, logic and echos so far.

--- SherrySwanseyFinal/Chat.php
+++ SherrySwanseyFinal/Chat.php
@@ -8,6 +8,13 @@
       $src= $_GET['Totalsearch'];
       header("Location: SearchResults.php?Totalsearch=".$src."");
     }
+    $threadID = 0;
+    if(isset($_GET['ThreadID']))
+    {
+        $threadID = $_GET['ThreadID'];
+        $thread = getAllMessagesInThread($threadID);
+    }
+    $userID = getUserID($_SESSION['email']);
 ?>
 
 <!DOCTYPE html>
@@ -21,24 +28,40 @@
     <div class="container gz-div-glow">
         <div class="container gz-div-inner mx-auto text-center py-5 text-white" style="font-family: textFont;">
             <div class="row border border-white rounded m-2" style="background-image: radial-gradient(ellipse at center, #e75480,#f71a08)">
-                <div class="text-right" style="font-family: textFont; width:100%;margin-right:50px; margin-top:20px; color:black">
-                    <div style="background-color:white;border-radius:8px;border:3px inset silver; text-align:left;width:50%;float:right;padding:10px;">
-                        Message
-                        <div style="font-size:10px; marigin-left:10px;">
-                            -Sent by user at 2:35PM 2/20/2021
-                        </div>
-                    </div>
-                </div>
-                <div class="text-left" style="font-family: textFont; width:100%;margin-left:50px; margin-top:20px; color:black">
-                    <div style="background-color:white;border-radius:8px;border:3px inset silver; text-align:left;width:50%;padding:10px;">
-                        Response
-                        <div style="font-size:10px; marigin-left:10px;">
-                            -Sent by admin at 2:55PM 2/20/2021
-                        </div>
-                    </div>
-                </div>
-                <textarea style="width:100%; margin:40px;background-color:white; border-radius:15px; border:3px inset silver;" rows="5" cols="100">
-                </textarea>
+                <?php
+                    if($threadID != 0)
+                    {
+                        foreach($thread as $message)
+                        {
+                            if($message['Sender_ID'] == $userID)
+                            {
+                                echo '<div class="text-right" style="font-family: textFont; width:100%;margin-right:50px; margin-top:20px; color:black">';
+                                    echo '<div style="background-color:white;border-radius:8px;border:3px inset silver; text-align:left;width:50%;float:right;padding:10px;">';
+                                        echo 'Message';
+                                        echo '<div style="font-size:10px; marigin-left:10px;">';
+                                            echo '-Sent by user at 2:35PM 2/20/2021';
+                                        echo '</div>';
+                                    echo '</div>';
+                                echo '</div>';
+                            }
+                            else
+                            {
+                                echo '<div class="text-left" style="font-family: textFont; width:100%;margin-left:50px; margin-top:20px; color:black">';
+                                    echo '<div style="background-color:white;border-radius:8px;border:3px inset silver; text-align:left;width:50%;padding:10px;">';
+                                        echo 'Response';
+                                        echo '<div style="font-size:10px; marigin-left:10px;">';
+                                            echo '-Sent by admin at 2:55PM 2/20/2021';
+                                        echo '</div>';
+                                    echo '</div>';
+                                echo '</div>';
+                            }
+                        }
+                    }
+                ?>
+                <form method="post" style="width:90%;">
+                    <textarea name="message" style="padding:5px; width:100%; margin:40px;background-color:white; border-radius:15px; border:3px inset silver;" rows="5" cols="100" text=""></textarea>
+                    <input style="margin-bottom:20px;margin-top:-20px;float:right;" type="submit" text="send">
+                </form>
             </div>
         </div>
     </div>
