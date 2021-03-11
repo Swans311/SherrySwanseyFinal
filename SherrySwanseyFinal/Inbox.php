676901f2c2bae2500af6ed269783c8e7def6e695
FieryXJoe <josephmsherry@comcast.net>
2021-03-11 13:31:01 -0500

    Style changes to Chat and inbox

--- SherrySwanseyFinal/Inbox.php
+++ SherrySwanseyFinal/Inbox.php
@@ -44,29 +44,29 @@
                 {
                     echo '<a href = "Chat.php?ThreadID=' . $threadLastMessage['Thread_ID'] . '" style="text-decoration: inherit;color: inherit; cursor: auto;">';
                         echo '<div class="row border border-white rounded m-2" style="background-image: radial-gradient(ellipse at center, #e75480,#f71a08)">';
-                            echo '<div class="media mx-3" style="padding-top: 15px; padding-bottom: 15px;">';
-                                        echo '<div class="col-md-3">';
-                                            echo '<p class="text-left">';
-                                                echo 'Topic <br>';
-                                                echo '<hr style="width:100%!important; border-top:2px solid white;"/>';
-                                                echo '&emsp;' . $threadLastMessage['Topic'] . '<br>';
-                                                /* remove or get origin message echo '&emsp; Date Opened';*/
+                            echo '<div class="media mx-3" style="padding-top: 15px; padding-bottom: 15px;width:100%;">';
+                                    echo '<div class="col-md-3">';
+                                        echo '<p class="text-left">';
+                                            echo 'Topic <br>';
+                                            echo '<hr style="width:100%!important; border-top:2px solid white;"/>';
+                                            echo '&emsp;' . $threadLastMessage['Topic'] . '<br>';
+                                            /* remove or get origin message echo '&emsp; Date Opened';*/
+                                        echo '</p>';
+                                    echo '</div>';
+                                    echo '<div id="row" style="width:100%;">';
+                                        echo '<div class="col-md-12" style="margin-top:35px;">';
+                                            echo '<p>';
+                                                echo $threadLastMessage['Message'];
                                             echo '</p>';
                                         echo '</div>';
-                                        echo '<div id="row">';
-                                            echo '<div class="col-md-12" style="margin-top:35px;">';
-                                                echo '<p>';
-                                                    echo $threadLastMessage['Message'];
-                                                echo '</p>';
-                                            echo '</div>';
+                                    echo '</div>';
+                                    echo '<div id="row">';
+                                        echo '<div class="col-md-15" style="margin-top:35px;">';
+                                            echo '<p>';
+                                                echo $threadLastMessage['TimeSent'];
+                                            echo '</p>';
                                         echo '</div>';
-                                        echo '<div id="row">';
-                                            echo '<div class="col-md-3" style="margin-top:35px;">';
-                                                echo '<p>';
-                                                    echo $threadLastMessage['TimeSent'];
-                                                echo '</p>';
-                                            echo '</div>';
-                                        echo '</div>';
+                                    echo '</div>';
                             echo '</div>';
                         echo '</div>';
                     echo '</a>';
@@ -76,9 +76,9 @@
         </div>
     </div>
 </body>
-<footer style="bottom:0; width:100%;">
+<footer style="bottom:0;  width:100%;">
   <br/>
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.4); color:red; font-size:16px">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.9); color:#ff3300; font-size:16px">
     © 2021 Copyright:
     <a class="text-blue" href="AboutUs.php">About Us</a>
   </div>
