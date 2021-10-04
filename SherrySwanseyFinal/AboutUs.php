55afa1bdb6c43ea738ffd55f4b5b97d739a7ceca
Swans311 <DSwansey@email.neit.edu>
2021-10-04 19:39:49 -0400

    changes

--- SherrySwanseyFinal/AboutUs.php
+++ SherrySwanseyFinal/AboutUs.php
@@ -40,7 +40,7 @@
                         </div>
                         <img class="mr-3 align-self-top" style="height: auto; width: 28%;" src="misc\images\JoeSBio.jpg" alt="img">
                     </div>
-                    <p>Primarily handled the PHP data access layer. I did a majority of the frontend PHP and JS, and Adding reviews. Finally communicated with my team to ensure that the backend SQL & frontend HTML had everything they should and nothing they shouldn't have to work with our functions and functionality we were hoping to achieve. Made sure the full stack could work together in theory and picked up work on all ends to make sure it did in reality</p>
+                    <p>Primarily handled the PHP data access layer. I did a majority of the frontend PHP and JS, and Adding reviews. Finally communicated with my team to ensure that the backend SQL & frontend HTML had everything they should and nothing they shouldn't have to work with our functions and functionality we were hoping to achieve. Made sure the full stack could work together in theory and picked up work on all ends to make sure it did in reality.</p>
                 </div>
                 <div class="col px-1 border-right border-white">
                     <div class="media mr-auto">
@@ -53,7 +53,7 @@
                         </div>
                         <img class="mr-3 align-self-top" style="height: auto; width: 28%;" src="misc\images\Swansey.jpg" alt="img">
                     </div>
-                    <p>My part of this project was managing the SQL database and adjusting the tables to fit the data. As well as that, I also built and implemented the login/logout, Signup, and account viewing systems in PHP and JS. I also built the homepage and functional buttons on it as well. I was mainly responsible for a majority of frontend PHP and any/all SQL and the implentation of it. I also had constructed the webpages using Casey's designs and style code as a reference</p>
+                    <p>My part of this project was managing the SQL database and adjusting the tables to fit the data. As well as that, I also built and implemented the login/logout, Signup, and account viewing systems in PHP and JS. I also built the homepage and functional buttons on it as well. I was responsible for a majority of frontend PHP and any/all SQL and the implentation of it. I also had constructed the webpages using Casey's designs and style code as a reference. Created Search page and functionality, as well as being able to view the restaurant menu from the restaurant's individual page. Kept the project on track through the course.</p>
                 </div>
                 <div class="col px-1">
                     <div class="media mr-auto">
